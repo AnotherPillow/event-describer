4f0c0598bd0319d7789ab02758b7a76f04533cf2
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2026-07-16 04:57:37 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 <svelte:head>
     <link rel="icon" href={favicon} />
    <meta content="Event Describer" property="og:title" />
-    <meta content={favicon} property="og:image" />
+    <meta content="https://event-describer.pillow.rocks/puffernotes.png" property="og:image" />
     <meta content="Automatically generate documentation for your Stardew Valley destivals by uploading their JSON source" property="og:description" />
     <meta content="https://event-describer.pillow.rocks" property="og:url" />
     <meta content="#FF2C29" name="theme-color" />
